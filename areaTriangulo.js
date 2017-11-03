4ff129b5585d66d4e9796c8afa1bb51a31a6693f
Carlos Balanzario <carlosb@dgp.unam.mx>
2017-11-03 12:56:00 -0600

    Curso Principios de Javascript.

---
+++ areaTriangulo.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 7;
+
+function areaTriangulo(base, altura){
+  return base * altura / 2
+}
+
+console.log(`El área de un triángulo de Base ${base} y altura ${altura} es: ${areaTriangulo(base, altura)}`);
