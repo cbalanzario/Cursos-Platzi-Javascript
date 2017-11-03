6aa5d642d6510214bb472f7c8c4022d3a7f150be
Carlos Balanzario <carlosb@dgp.unam.mx>
2017-11-03 13:57:00 -0600

    Quitando la palabra function. Usando igual y mayor que

--- areaTriangulo.js
+++ areaTriangulo.js
@@ -1,10 +1,7 @@
 let base = 5;
 let altura = 7;
 
-let areaTriangulo = (base, altura) => {
-  return base * altura / 2
-}
-
+let areaTriangulo = (base, altura) => base * altura / 2
 
 console.log(`El área de un triángulo de Base ${base} y altura ${altura} es:
-  ${areaTriangulo(base, altura)}`);
+${areaTriangulo(base, altura)}`);
