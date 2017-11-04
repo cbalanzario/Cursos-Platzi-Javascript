177aef34912cdabf632f16c50736a7a9954c9830
Carlos Balanzario <carlosb@dgp.unam.mx>
2017-11-03 18:30:42 -0600

    Poniendo como constante la función de areaTriangulo

--- areaTriangulo.js
+++ areaTriangulo.js
@@ -1,7 +1,7 @@
 let base = 5;
 let altura = 7;
 
-let areaTriangulo = (base, altura) => base * altura / 2
+const areaTriangulo = (base, altura) => base * altura / 2
 
 console.log(`El área de un triángulo de Base ${base} y altura ${altura} es:
 ${areaTriangulo(base, altura)}`);
