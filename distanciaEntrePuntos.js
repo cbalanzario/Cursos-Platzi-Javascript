9f5d4d905a8e2b3be379b6267380b9781d9a438c
Carlos Balanzario <carlosb@dgp.unam.mx>
2017-11-07 11:28:09 -0600

    Tercer método - Usando clases en javascript, sintaxis-sugar.

--- distanciaEntrePuntos.js
+++ distanciaEntrePuntos.js
@@ -1,47 +1,24 @@
-// function Punto(x,y) {//Creamos un prototipo, así podrá ser repetidos
-//     this.x = x
-//     this.y = y
-// }
-//
-// Punto.prototype.moverEnX = function moverEnX(x){
-//     this.x += x
-// }
-//
-// Punto.prototype.moverEnY = function moverEnY(y){
-//     this.y += y
-// }
-//
-// Punto.prototype.distancia = function distancia(p){
-//     const x = this.x - p.x
-//     const y = this.y - p.y
-//
-//     return Math.sqrt(x*x + y*y)
-// }
-const Punto = {
-    init: function init(x,y) {
+class  Punto {//Creamos la clase punto, funciona como un prototipo
+    constructor(x,y) {
         this.x = x
         this.y = y
-    },
-    moverEnX: function moverEnX(x) {
-        this.x += x
-    },
-    moverEnY: function moverEnY(y) {
-        this.y += y
-    },
-    distancia: function distancia(p) {
-        const x = this.x - p.x
-        const y = this.y - p.y
-
-        return Math.sqrt(x*x + y*y)
     }
+    moverEnX(x){
+        this.x += x
+    }
+    moverEnY(y){
+        this.y += y
+    }
+    distancia(p){
+       const x = this.x - p.x
+       const y = this.y - p.y
+
+       return Math.sqrt(x*x + y*y)
+   }
 }
 
-
-const p1 = Object.create(Punto)
-const p2 = Object.create(Punto)
-p1.init(0,4)
-p2.init(3,0)
-
+const p1 = new Punto (0,4)
+const p2 = new Punto (3,0)
 
 console.log(p1.distancia(p2))
 console.log(p2.distancia(p1))
