306ec6ed6b5f9230a9b4bd669710618530b346ed
Carlos Balanzario <carlosb@dgp.unam.mx>
2017-11-07 19:27:58 -0600

    Así se traduce la parte del for para los dobles a la función map. Ahora hay que reducirlo...

--- operacionesArrays.js
+++ operacionesArrays.js
@@ -1,12 +1,21 @@
-function suma(...numeros) {//Método tradicional
-    // let acumulados = 0
-    // for(let i=0; i < numeros.length; i++) {
-    //     acumulados += numeros[i]
-    // }
-    return numeros.reduce(function (acumulados, numero) {
+function suma(...numeros) {
+    return numeros.reduce(function (acumulados, numero) {//El método reduce() aplica una
+                                                        //función a un acumulador y a cada valor de un
+                                                        //array (de izquierda a derecha) para reducirlo
+                                                        //a un único valor
         acumulados += numero
         return acumulados
     }, 0)
 }
+
+function dobles(...numeros) {//Función que nos devolverá el doble de sus números en el array
+    /*Código escrito correctamente, aunque se puede reducir y hacerlo más elegante*/
+    const resultado = []
+    for (let i=0; i<numeros.length; i++){
+        resultado.push(numeros[i]*2)
+    }
+    return resultado
+}
+
 
 suma (4, 8, 12, 8954, 7)
