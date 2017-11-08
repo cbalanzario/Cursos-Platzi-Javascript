0c4ffd890e43cf621ff2e68433864223083fd9d6
Carlos Balanzario <carlosb@dgp.unam.mx>
2017-11-08 13:54:39 -0600

    Terminado las operaciones con funciones dentro de Arrays

--- operacionesArrays.js
+++ operacionesArrays.js
@@ -1,16 +1,9 @@
-function suma(...numeros) {
-    return numeros.reduce(function (acumulados, numero) {//El método reduce() aplica una
-                                                        //función a un acumulador y a cada valor de un
-                                                        //array (de izquierda a derecha) para reducirlo
-                                                        //a un único valor
-        acumulados += numero
-        return acumulados
-    }, 0)
-}
+//El método reduce() aplica una función a un acumulador y a cada valor de un
+//array (de izquierda a derecha) para reducirlo a un único valor
+const suma = (...numeros) => numeros.reduce((acumulados, numero)=> acumulados += numero,0)
 
 //Función que nos devolverá el doble de sus números en el array
 const dobles = (...numeros) => numeros.map(numero => numero *2 ) //map recibe una función que se va a ejecutar por cada uno de los elementos
-
 
 //otro método de array es filter, lo podemos usar para encontrar elementos en el arreglo
 const pares = (...numeros) => numeros.filter(numero => numero % 2 == 0)
